22aae9004e1de7d216468e757e207778caee2aed
Guled <somnibyte@gmail.com>
2017-04-07 10:50:53 -0500

    Neural Network prototype complete.

---
+++ MLKit/Classes/ANN/ActivationFunctionType.swift
@@ -0,0 +1,9 @@
+//
+//  ActivationFunctionType.swift
+//  Pods
+//
+//  Created by Guled  on 4/7/17.
+//
+//
+
+import Foundation
---
+++ MLKit/Classes/ANN/InputDataType.swift
@@ -0,0 +1,9 @@
+//
+//  InputDataType.swift
+//  Pods
+//
+//  Created by Guled  on 4/7/17.
+//
+//
+
+import Foundation
